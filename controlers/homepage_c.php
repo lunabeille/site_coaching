121c594e54c7155efd75d63224c985984fcc0e54
lunabeille <lucy.bouquet@gmail.com>
2016-04-04 16:49:21 +0200

    Modification homepage_c.php

--- controlers/homepage_c.php
+++ controlers/homepage_c.php
@@ -1,8 +1,13 @@
 <?php
 include('../lib/utils.php');
 
+//récupère les infos du coureur à afficher dans le profil
 function homepage_controler(){
   $params = get_infos(4);
   return $params;
 }
+
+//mettre à jour les données du profil
+//function update_profile(){
+//}
 ?>
