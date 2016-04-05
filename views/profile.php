cc4cfcba143c8322a3e9fecd3151f17d8d195312
lunabeille <lucy.bouquet@gmail.com>
2016-04-05 14:55:30 +0200

    correction formulaire bis

--- views/profile.php
+++ views/profile.php
@@ -10,24 +10,22 @@
   <?php 
   echo('<p>Voici la page d\'acceuil de profil du coureur</p>');
   ?>
-  <form>
-    <fieldset>
-      <legend>Informations générales</legend>
-        <div> Prénom : <?php echo $nom; ?></div>
-        <div> Age : <?php echo $age; ?></div>
-        <div> Ville : <?php echo $ville; ?></div>
-    </fieldset>
-  </form>
+  
+  <fieldset>
+     <legend>Informations générales</legend>
+      <div> Prénom : <?php echo $nom; ?></div>
+      <div> Age : <?php echo $age; ?></div>
+      <div> Ville : <?php echo $ville; ?></div>
+  </fieldset>
 
-  <form>
-    <fieldset>
-      <legend>Profil coureur</legend>
-        <div> VMA: <?php echo $vma; ?></div>
-        <div> Meilleure perf 10km : <?php echo $rp10 ?></div>
-        <div> Meilleure perf Semi : <?php echo $rpsemi ?></div>
-        <div> Meilleure perf marathon : </div>
-    </fieldset>
-  </form>
+  <fieldset>
+    <legend>Profil coureur</legend>
+      <div> VMA: <?php echo $vma; ?></div>
+      <div> Meilleure perf 10km : <?php echo $rp10 ?></div>
+      <div> Meilleure perf Semi : <?php echo $rpsemi ?></div>
+      <div> Meilleure perf marathon : </div>
+  </fieldset>
+  
   <p> <a href="results.php">Lien vers les résultats </a></p>
   <p> <a href="profile_form.php"> Modifier mon profil </a></p>
   <p> <a href="news.php">Lien vers les News </a></p>
