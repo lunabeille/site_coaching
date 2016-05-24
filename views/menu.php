7ed3f9fca07cab73d0a29dab4bd95c72e38a007e
lunabeille <lucy.bouquet@gmail.com>
2016-05-24 13:06:46 +0200

    Modification des liens dans le menu : passage en relatif

--- views/menu.php
+++ views/menu.php
@@ -1,10 +1,10 @@
 <div id="menu">
   <ul>
-    <li><a href="/sitecoaching/index.php/displayProfile">Home</a></li>
-    <li><a href="/sitecoaching/index.php/displayResults">Mes résultats </a></li>
-    <li><a href="/sitecoaching/index.php/updateProfile"> Modifier mon profil </a></li>
-    <li><a href="/sitecoaching/index.php/news">News </a></li>
-    <li><a href="/sitecoaching/index.php/calendar">Calendrier des courses </a></li>
+    <li><a href="./displayProfile">Home</a></li>
+    <li><a href="./displayResults">Mes résultats </a></li>
+    <li><a href="./updateProfile"> Modifier mon profil </a></li>
+    <li><a href="./news">News </a></li>
+    <li><a href="./calendar">Calendrier des courses </a></li>
   </ul>
 </div>
 
