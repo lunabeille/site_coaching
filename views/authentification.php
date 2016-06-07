ec6daed545a54992fc5dea4cd7961c4943a59b94
lunabeille <lucy.bouquet@gmail.com>
2016-06-07 09:56:05 +0200

    Amléioration de la mise en forme CSS de la page d'authentification

--- views/authentification.php
+++ views/authentification.php
@@ -2,14 +2,13 @@
   session_start(); 
   ?>
   <div class="content">
-  <h3> Bienvenue sur le site de Coachette ! </h3>
+  <h3 id="welcome"> Bienvenue sur le site de coachette ! </h3>
 
   <form method="POST" action="/sitecoaching/index.php/authentification">
   <fieldset class="login-block">
-     <legend>  CONNEXION </legend>
-      <div> <img src="../views/runner.png" width="30" class="input-logo"><input type="text" name ="login" class="champ" placeholder="LOGIN"/> </div>
-      <div> <img src="../views/lock.jpeg" width="30" class="input-logo"><input type="text" name ="passwd" class="champ" placeholder="PASSWORD"/> </div>
-      <input type="submit" id ="login" name="login" value="LOG IN">
+      <div> <img src="../views/runner2.png" width="25" class="input-logo"><input type="text" name ="login" class="champ" placeholder="LOGIN"/> </div>
+      <div> <img src="../views/lock.png" width="25" class="input-logo"><input type="text" name ="passwd" class="champ" placeholder="PASSWORD"/> </div>
+      <input type="submit" id ="login" name="login" value="CONNEXION">
 
   </fieldset>
 </form>
